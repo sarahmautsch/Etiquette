b7b20d30ff6ce9fcfa2300033e3e1c77abe74fb9
Sarah Mautsch <sarah.mautsch@icloud.com>
2019-03-02 17:00:03 +0100

    Initial Commit

---
+++ Etiquette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Etiquette
+//
+//  Created by Sarah Mautsch on 02.03.19.
+//  Copyright © 2019 Sarah Mautsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
